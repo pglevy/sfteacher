04914eaf7da80653c4dbce6f84030ffc9503b3cf
JoshBraun <josh.braun@hobsons.com>
2016-06-07 11:57:54 -0700

    Add CourseController

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -10,6 +10,18 @@
     'foundation.dynamicRouting',
     'foundation.dynamicRouting.animations'
   ])
+    .controller('CourseController',
+    ["$scope", "$state", "$http",function($scope, $state, $http){
+    $http.get("http://localhost:8081/courses", {cache:true})
+        .success(function(data){
+          console.log(data);
+          $scope['courses'] = data;
+        })
+        .error(function(data){
+          console.log(data);
+          $scope['courses'] = 'Failed'
+        })
+  }])
     .config(config)
     .run(run)
   ;
