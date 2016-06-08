eb5dc2dece49430b084369ba091fb28b931bf908
Darren Mason <djmason9@gmail.com>
2016-06-08 09:18:01 -0400

    updated images

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -26,7 +26,7 @@
   .controller('SurveyController',
     ["$scope", "$state", "$http",function($scope, $state, $http){
 
-    $http.get("http://localhost:8081/surveys", {params: params, cache:true})
+    $http.get("http://localhost:8081/surveys", {cache:true})
         .success(function(data){
           console.log(data);
           $scope['surveys'] = data;
@@ -39,11 +39,41 @@
 
     .controller('AttendanceController',
     ["$scope", "$state", "$http",function($scope, $state, $http){
-  $scope.id = ($state.params.id || '');
 
-  var params = {
-    'courseId' : $scope.id
-  }
+  $scope.id = ($state.params.id || '');
+  $scope.attendance = ($state.params.attendance || '');
+  $scope.studentId = ($state.params.studentId || '');
+  $scope.post = ($state.params.post || false);
+
+  console.log("is studentId? "+$scope.studentId);
+  var params = {};
+
+    if($scope.post){
+
+    $http.headers['Access-Control-Allow-Origin'] = '*';
+    $http.headers["Access-Control-Allow-Headers"] = "X-Requested-With";
+    $http.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT';
+
+    params = {
+    'status' : $scope.attendance,
+    'studentId' : $scope.studentId
+    };
+
+    $http.post("http://localhost:8081/updateAttendance", {withCredentials: true, params: params, cache:true})
+        .success(function(data){
+          console.log(data);
+
+        })
+        .error(function(data){
+          console.log(data);
+
+        })
+     }else{
+
+    params = {
+     'courseId' : $scope.id
+    };
+
     $http.get("http://localhost:8081/student/course", {params: params, cache:true})
         .success(function(data){
           console.log(data);
@@ -53,6 +83,7 @@
           console.log(data);
           $scope['students'] = 'Failed'
         })
+    }
   }])
     .config(config)
     .run(run)
--- sfteacher.js
+++ sfteacher.js
@@ -34,12 +34,12 @@ db.serialize(function() {
 
 
 //insert students
-  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Darren Mason','dmason1234','dmason','darren.mason','SCI-BIOL202-600-201602','PRESENT','2016-06-07 10:00:00','http://placehold.it/200x200')");
-  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Philip Levy','philiplevy1234','philiplevy','philip.levy','HIST-HIST301-600-201602','PRESENT','2016-06-07 10:00:00','http://placehold.it/200x200')");
-  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Josh Braun','joshbraun5005','joshbraun','josh.braun','SCI-BIOL202-600-201602','ABSENT','2016-06-07 10:00:00','http://placehold.it/200x200')");
-  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Steve Jobs','sjobs1234','sjobs','steve.jobes','SCI-BIOL101-500-201601','ABSENT','2016-05-07 10:00:00','http://placehold.it/200x200')");
-  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Aaron Jones','arronjones1234','aaronjones','arron.jones','HIST-HIST301-600-201602','PRESENT','2016-06-07 10:00:00','http://placehold.it/200x200')");
-  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Steve Day','steveday1234','steveday','steve.day','SCI-BIOL202-600-201602','PRESENT','2016-06-07 10:00:00','http://placehold.it/200x200')");
+  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Darren Mason','dmason1234','dmason','darren.mason','SCI-BIOL202-600-201602','PRESENT','2016-06-07 10:00:00','https://s3.amazonaws.com/uifaces/faces/twitter/rem/128.jpg')");
+  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Philip Levy','philiplevy1234','philiplevy','philip.levy','HIST-HIST301-600-201602','PRESENT','2016-06-07 10:00:00','https://s3.amazonaws.com/uifaces/faces/twitter/c_southam/128.jpg')");
+  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Josh Braun','joshbraun5005','joshbraun','josh.braun','SCI-BIOL202-600-201602','ABSENT','2016-06-07 10:00:00','https://s3.amazonaws.com/uifaces/faces/twitter/dancounsell/128.jpg')");
+  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Steve Jobs','sjobs1234','sjobs','steve.jobes','SCI-BIOL101-500-201601','ABSENT','2016-05-07 10:00:00','https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg')");
+  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Aaron Jones','arronjones1234','aaronjones','arron.jones','HIST-HIST301-600-201602','PRESENT','2016-06-07 10:00:00','https://s3.amazonaws.com/uifaces/faces/twitter/connor_gaunt/128.jpg')");
+  db.run("INSERT INTO " + TABLE_STUDENTS + "('student_name','student_id','student_username','student_integration_id','course_id','attendance_status','last_updated_date','image') VALUES ('Steve Day','steveday1234','steveday','steve.day','SCI-BIOL202-600-201602','PRESENT','2016-06-07 10:00:00','https://s3.amazonaws.com/uifaces/faces/twitter/teleject/128.jpg')");
 
 //insert courses
 	db.run("INSERT INTO " + TABLE_COURSES + "('date', 'time','course_name','course_id','present_students','absent_students','excused_students','tardy_students') VALUES ('2016-06-07 10:00:00','10:00:00 AM','Microbiology II','SCI-BIOL202-600-201602','49','2','1','3')");
@@ -89,6 +89,7 @@ app.get('/surveys', function (req, res) {
 
 app.post('/updateAttendance', jsonParser, function (req, res){
 
+  req.header("Access-Control-Allow-Origin", "*");
   if (!req.body) return res.sendStatus(400);
 
 	updateAttendance(req,res);
